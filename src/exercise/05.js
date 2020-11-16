b8c9d9b60f6deaa6690529023d95937fb4362058
karl-thomas <karl.thomas925@gmail.com>
2020-11-15 23:01:29 -0600

    lazy load the pokemon info component

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,6 +12,10 @@ import {
 } from '../pokemon'
 import {createResource} from '../utils'
 
+const PokemonInfo = React.lazy(() =>
+  import('../lazy/pokemon-info-render-as-you-fetch'),
+)
+
 function preloadImage(src) {
   return new Promise(resolve => {
     const img = document.createElement('img')
@@ -20,17 +24,17 @@ function preloadImage(src) {
   })
 }
 
-function PokemonInfo({pokemonResource}) {
-  const pokemon = pokemonResource.data.read()
-  return (
-    <div>
-      <div className="pokemon-info__img-wrapper">
-        <img src={pokemonResource.image.read()} alt={pokemon.name} />
-      </div>
-      <PokemonDataView pokemon={pokemon} />
-    </div>
-  )
-}
+// function PokemonInfo({pokemonResource}) {
+//   const pokemon = pokemonResource.data.read()
+//   return (
+//     <div>
+//       <div className="pokemon-info__img-wrapper">
+//         <img src={pokemonResource.image.read()} alt={pokemon.name} />
+//       </div>
+//       <PokemonDataView pokemon={pokemon} />
+//     </div>
+//   )
+// }
 
 const SUSPENSE_CONFIG = {
   timeoutMs: 4000,
